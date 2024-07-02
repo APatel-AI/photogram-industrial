c788d327c2284b02e2ac65af0d3223a9c9ff4633
apatel-ai <arpan.p.se@gmail.com>
2024-07-02 11:59:02 -0500

    updated like model validation and added destroy to handle orphan records

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,13 +29,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
-  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest", dependent: :destroy
+  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest", dependent: :destroy
 
   has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
 
-  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest", dependent: :destroy
   has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
+
 
   has_many :likes, foreign_key: :fan_id
   has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
@@ -46,6 +47,8 @@ class User < ApplicationRecord
 
   has_many :comments, foreign_key: "author_id"
   validates :username, presence: true, uniqueness: true
+
+
 
 
 
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -9,6 +9,22 @@ task({ :sample_data => :environment }) do
     Photo.destroy_all
     User.destroy_all
   end
+
+  12.times do
+    name = Faker::Name.first_name
+    u = User.create(
+      email: "#{name}@example.com",
+      password: "password",
+      username: name,
+      private: [true, false].sample,
+    )
+
+    p u.errors.full_messages
+  end
+
+  p "There are now #{User.count} users."
+
+
   users = User.all
 
   users.each do |user|
